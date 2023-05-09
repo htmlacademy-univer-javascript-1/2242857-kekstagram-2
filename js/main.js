3495851478b09a9579e9323e3470d21c2f71b8cb
astriaporta42 <x@ekt.ru>
2023-05-10 00:18:00 +0500

    8.19. Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {NUMBER_OF_PICTURES, generatePictureDescription} from './data.js';
 import {addPictures} from './picture.js';
+import './upload-image.js';
 
 const pictureDescriptions = Array.from({length: NUMBER_OF_PICTURES}, generatePictureDescription);
 addPictures(pictureDescriptions);
