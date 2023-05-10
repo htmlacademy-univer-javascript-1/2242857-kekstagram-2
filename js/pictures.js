1da9427df2ea758261b6b84c4c1d8585bba10de6
astriaporta42 <x@ekt.ru>
2023-05-10 05:36:06 +0500

    12.11. Перламутровые пуговицы

--- js/pictures.js
+++ js/pictures.js
@@ -1,17 +1,21 @@
 import {getPictures} from './api.js';
 import {openBigPicture} from './big-picture.js';
 import {showError} from './messages.js';
+import {UniqueRandomNumberGenerator, debounce} from './util.js';
+
+const filtersContainer = document.querySelector('.img-filters');
 
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture')
 
-let pictureDescriptions = new Map();
+let elementToPicture = new Map();
+let pictures = null;
 
 const onClickHandler = (event) => {
   event.preventDefault();
   
   const picture = event.target.closest('A.picture');
-  const pictureDescription = pictureDescriptions.get(picture);
+  const pictureDescription = elementToPicture.get(picture);
   
   openBigPicture(pictureDescription);
 };
@@ -28,7 +32,7 @@ export const addPictures = (pictures) => {
     pictureFragment.querySelector('.picture__comments').textContent = picture.comments.length;
     
     pictureElement.addEventListener('click', onClickHandler);
-    pictureDescriptions.set(pictureElement, picture);
+    elementToPicture.set(pictureElement, picture);
     
     fragment.appendChild(pictureFragment);
   });
@@ -36,4 +40,51 @@ export const addPictures = (pictures) => {
   picturesContainer.appendChild(fragment);
 };
 
-getPictures(addPictures, showError);
+export const clearPictures = () => {
+  picturesContainer.querySelectorAll('.picture').forEach((pictureElement) => pictureElement.remove());
+  elementToPicture.clear();
+};
+
+export const showPictures = debounce((pictures) => {
+  clearPictures();
+  addPictures(pictures);
+}, 500);
+
+const onChangeFilter = (event) => {
+  filtersContainer.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  const filterElement = event.target.closest('.img-filters__button');
+  filterElement.classList.add('img-filters__button--active');
+  
+  if (filterElement.id === 'filter-default') {
+    showPictures(pictures);
+  } else if (filterElement.id === 'filter-random') {
+    const generator = new UniqueRandomNumberGenerator(0, pictures.length - 1);
+    const filteredPictures = [];
+    for (let i = 0; i < 10; i++) {
+      filteredPictures.push(pictures[generator.generate()]);
+    }
+    showPictures(filteredPictures);
+  } else if (filterElement.id === 'filter-discussed') {
+    const sortedPictures = new Array(...pictures);
+    sortedPictures.sort((a, b) => {
+      if (a.comments.length > b.comments.length) {
+        return -1;
+      }
+      if (a.comments.length < b.comments.length) {
+        return 1;
+      }
+      return 0;
+    });
+    showPictures(sortedPictures);
+  }
+};
+
+getPictures((picturesParameter) => {
+  pictures = picturesParameter;
+  if (picturesParameter.length > 0) {
+    filtersContainer.classList.remove('img-filters--inactive');
+    filtersContainer.querySelectorAll('.img-filters__button').forEach((filter) => filter.addEventListener('click', onChangeFilter));
+    
+    showPictures(pictures);
+  }
+}, showError);
